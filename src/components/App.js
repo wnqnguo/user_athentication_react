2e335d43c5ae0dd835145a1e258be1135c0165b7
Wenqian Guo <wnqnguo@gmail.com>
2016-08-31 16:45:06 -0700

    app component

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import 'normalize.css/normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { Component } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import React from 'react';
+
+
+
+class AppComponent extends Component {
+
+  componentWillMount() {
+    this.lock = new Auth0Lock('YOUR_AUTH0_CLIENT_ID', 'YOUR_AUTH0_DOMAIN);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header lock={this.lock}></Header>
+        <Grid>
+          <Row>
+            <Col xs={12} md={3}>
+              <Sidebar />
+            </Col>
+            <Col xs={12} md={9}>
+              {this.props.children}
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
